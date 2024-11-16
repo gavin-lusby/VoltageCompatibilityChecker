f7cd5255397ca5225017f750a8cb899340a8adb1
gavin-lusby <107567936+gavin-lusby@users.noreply.github.com>
2024-11-15 22:44:07 -0500

    Update license and TODO

--- app.py
+++ app.py
@@ -15,7 +15,5 @@ Add trapezoid red/green indicating if devices are compatible
 add red text hightlighting(if possible) to dropdown to
 indicate device to be selected will not be compatible
 with currently selected OTHER device
-Add save to CSV feature
-Add import from CSV feature
-cleanup import/export
-Add voltage linear equation instead of only VCC or only constant (x*Vcc + y)"""
+solve todos
+add scalable vcc slider"""
